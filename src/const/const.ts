09565ad20415b5e50fc87167ed8821195955471a
Solanki Gaurav <solankigaurav2107@gmail.com>
2025-10-08 14:37:08 +0530

    feat: simplify BASEURL definition by using window.location.origin for better clarity

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,3 +1,3 @@
 export const BASEURL = typeof window !== 'undefined'
-  ? `${window.location.protocol}//${window.location.hostname}:3000`
+  ? window.location.origin
   : process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
